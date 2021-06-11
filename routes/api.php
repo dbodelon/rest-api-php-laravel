eb8567d033a4d8ce981aac7cf2198e350c4e308c
Danilo Bodelon <danilobodelon@hotmail.com>
2021-06-11 12:13:44 -0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Listar todos
+Route::get('/produtos',[ProdutoController::class, 'index']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Criar
+Route::post('/produtos', [ProdutoController::class, 'store']);
+
+//Listar por id
+Route::get('/produtos/{id}', [ProdutoController::class, 'show']);
+
+//Alterar
+Route::put('/produtos/{id}', [ProdutoController::class, 'update']);
+
+//Deletar
+Route::delete('/produtos/{id}', [ProdutoController::class, 'destroy']);
+
